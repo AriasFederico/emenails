82b88325eaf5b8adc07d9d3205fdee19c7224a6c
Federico Arias <fede.ariass33@gmail.com>
2024-09-20 20:00:31 -0300

    cambiando icono de la pagina

--- src/components/header/common/Logo.jsx
+++ src/components/header/common/Logo.jsx
@@ -1,9 +1,8 @@
 import logo from '../../../../public/iconEMENails.png';
 export const Logo = ({ className }) => {
 	return (
-		<a href='/'>
+		<a href='/' className={className}>
 			<img src={logo} alt='' className={className} />
-			EMENails
 		</a>
 	);
 };
