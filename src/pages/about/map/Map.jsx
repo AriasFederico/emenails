9c3fc2db9992fd319ce37193c9d215b72a8b88de
Federico Arias <fede.ariass33@gmail.com>
2024-09-22 21:52:16 -0300

    acomodando mapa emenails

--- src/pages/about/map/Map.jsx
+++ src/pages/about/map/Map.jsx
@@ -13,7 +13,7 @@ export const Map = () => {
 					frameBorder='0'
 					marginHeight='0'
 					marginWidth='0'
-					src='https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=calle%2057%20entre%20139%20y%20140%20los%20hornos+(EMENails)&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed'
+					src='https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=%20calle%2057%20numero%2013%20los%20hornos+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed'
 				>
 					<a href='https://www.gps.ie/'>gps trackers</a>
 				</iframe>
